083db2843756a8723a36eb0ddfa6eca2f4972a49
Sébastien LECOCQ <sebastien.lecocq@nutanix.com>
2026-02-01 18:09:12 +0100

    Corrected bug in project creation task ('Project Admin' role not in first page results)

--- scripts/create-project.sh
+++ scripts/create-project.sh
@@ -3,4 +3,11 @@
 cd ~/.calm
 source venv/bin/activate
 
-python ~/ntnx-escape-game/scripts/create-project.py
+pc_ip = "@@{PC_IP}@@"
+pc_user = "@@{PC_USERNAME}@@"
+pc_password = "@@{PC_PASSWORD}@@"
+
+primary_subnet_name = "@@{PRIMARY_SUBNET}@@"
+secondary_subnet_name = "@@{SECONDARY_SUBNET}@@"
+
+python ~/ntnx-escape-game/scripts/create-project.py --pcIp $pc_ip --pcUser $pc_user --pcPassword $pc_password --primarySubnet $primary_subnet_name --secondarySubnet $secondary_subnet_name
